318daa1b14599c083b9af0a0fb3ab3d6caef4ca1
unknown <shivamgupta1830@gmail.com>
2023-08-01 12:13:40 +0530

    Commit message

---
+++ src/components/CTA.jsx
@@ -0,0 +1,19 @@
+import Button from "./Button";
+import styles from "../style";
+
+const CTA = () => (
+  <section
+    className={` ${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Let's try our service now!</h2>
+      <p className={`${styles.paragraph} max-w[470px] mt-5`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit dolor obcaecati.
+      </p>
+    </div>
+    <div className={`${styles.flexCenter} sm:ml ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+);
+export default CTA;
